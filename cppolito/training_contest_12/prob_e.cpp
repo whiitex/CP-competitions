76a152ae8e5a7722f63c3a8824f8c262b24e2bb2
whiteOFF / Pasquale + Bianco <pasquale03.bianco@gmail.com>
2023-12-01 03:07:06 +0100

    dp with map data structure

--- cppolito/training_contest_12/prob_e.cpp
+++ cppolito/training_contest_12/prob_e.cpp
@@ -64,12 +64,12 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
-/*
-    int t;
-    cin >> t;
-    while (t--) {*/
+
+    int t = 1;
+//    cin >> t;
+    while (t--) {
         solve();
-    //}
+    }
 }
 
 
