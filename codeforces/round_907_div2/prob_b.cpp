ab00f59b8b6dcbad231a446d6acb3006dc44b9b0
whiteOFF / Pasquale + Bianco <pasquale03.bianco@gmail.com>
2024-01-03 23:33:51 +0100

    update

--- codeforces/round_907_div2/prob_b.cpp
+++ codeforces/round_907_div2/prob_b.cpp
@@ -3,69 +3,27 @@
 
 using namespace std;
 
-class Node {
-public:
-    ll a;
-    ll i;
-    vector<Node*> children;
-    Node(ll value, ll index) {
-        a = value;
-        i = index;
-    }
-};
-
-ll power(ll b, ll exp) {
-    while (exp--)
-        b *= 2;
-    return b;
-}
-
 void solve(){
     ll n, q;
     cin >> n >> q;
 
-    // init
-    ll a[n], x[q], powers[31];
-    for (ll i=0; i<n; i++)
-        cin >> a[i];
-    for (ll i=0; i<q; i++)
-        cin >> x[i];
-
-    // powers
-    powers[0] = 1;
-    for (ll i=1; i<31; i++)
-        powers[i] = power(2, i-1);
-
-    set<ll> logs[31];
-    for (ll i=1; i<n; i++) {
-        int pow = 30;
-        while (a[i] % powers[pow] != 0)
-            pow --;
-        if (pow != 0) logs[pow].insert(i);
+    vector<int> a(n), query;
+    for (int i=0; i<n; i++) cin >> a[i];
+    for (int i=0; i<q; i++) {
+        int temp; cin >> temp;
+        if (i == 0) query.push_back(temp);
+        else if (temp < *query.rbegin())
+            query.push_back(temp);
     }
 
-
-    // solve
-    ll my_x;
-    for (ll i=0; i<q; i++) {
-        my_x = x[i];
-        for (ll j: logs[my_x]) {
-            a[j] += powers[my_x-1];
-            ll pow = my_x;
-            while (a[j] % powers[pow] != 0)
-                pow --;
-            if (pow != 0) logs[pow].insert(j);
-        }
-        logs[my_x].clear();
+    for (int el: a) {
+        for (int two: query)
+            if (el % (1 << two) == 0)
+                el += (1 << (two - 1));
+        cout << el << ' ';
     }
-
-
-
-
-    // final print
-    for (ll i=0; i<n; i++)
-        cout << a[i] << ' ';
     cout << '\n';
+
 }
 
 int main() {
