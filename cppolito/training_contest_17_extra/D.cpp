9130934e8c56c2ba0e0f032d88d4945a8f806b4f
whiteOFF / Pasquale + Bianco <pasquale03.bianco@gmail.com>
2024-04-06 02:18:52 +0200

    cppolito training 17 extra

--- cppolito/training_contest_17_extra/D.cpp
+++ cppolito/training_contest_17_extra/D.cpp
@@ -1,54 +1,54 @@
-    #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 
-    using namespace std;
+using namespace std;
 
-    typedef vector<int> vi;
+typedef vector<int> vi;
 
-    #define pb push_back
+#define pb push_back
 
-    void solve() {
-        int n, k; cin >> n >> k;
+void solve() {
+    int n, k; cin >> n >> k;
 
-        vi assign(n);
-        int Ass = 1;
+    vi assign(n);
+    int Ass = 1;
 
-        for (int i=0; i<n;) {
+    for (int i=0; i<n;) {
 
-            int ass = min(Ass + k/2-1, Ass + ((n-Ass)/2));
-            int delta = ass - Ass;
-            int d = i;
-            while (ass >= Ass) {
-                assign[d] = ass--;
-                ++d;
-            }
-
-            ass = min(Ass + k - 1, n);
-            for (int d=i+delta+1; d < min(i+k, n); ++d)
-                assign[d] = ass--;
-
-            Ass += k;
-            i += k;
+        int ass = min(Ass + k/2-1, Ass + ((n-Ass)/2));
+        int delta = ass - Ass;
+        int d = i;
+        while (ass >= Ass) {
+            assign[d] = ass--;
+            ++d;
         }
 
-        for (int i: assign) cout << i << ' '; cout << '\n';
-        cout << ((n + k - 1) / k) << '\n';
+        ass = min(Ass + k - 1, n);
+        for (int d=i+delta+1; d < min(i+k, n); ++d)
+            assign[d] = ass--;
 
-        int cl = 1;
-        for (int i=0; i<n;) {
-            for (int d=i; d < min(i+k, n); ++d) {
-                cout << cl << ' ';
-            }
-            ++cl;
-            i += k;
+        Ass += k;
+        i += k;
+    }
+
+    for (int i: assign) cout << i << ' '; cout << '\n';
+    cout << ((n + k - 1) / k) << '\n';
+
+    int cl = 1;
+    for (int i=0; i<n;) {
+        for (int d=i; d < min(i+k, n); ++d) {
+            cout << cl << ' ';
         }
-        cout << '\n';
-
+        ++cl;
+        i += k;
     }
+    cout << '\n';
 
-    signed main() {
-        ios::sync_with_stdio(false);
-        cin.tie(0);
+}
 
-        int t; cin >> t;
-        while (t--) solve();
-    }
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int t; cin >> t;
+    while (t--) solve();
+}
